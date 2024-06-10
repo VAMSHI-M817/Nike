c8276cb2c3b024253f7e9e8b64ffbd905fc7953d
VAMSHI-M817 <vamshimarepu817@gmail.com>
2024-06-11 00:00:03 +0530

    commited

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -10,48 +10,41 @@ const Hero = () => {
 
   return (
     <section
-      id="home"
-      className="w-full flex 
-      xl:flex-row flex-col 
-      justify-center min-h-screen 
-      gap-10 max-container"
-    >
-      <div
-        className="relative
-      xl:w-2/5 flex-col justify-center items-start w-full max-xl:padding-x pt-28"
+        id='home'
+        className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'
       >
-        <p className="text-xl font-montserrat text-coral-red">
-          Our Summer collections
-        </p>
-        <h1
-          className="mt-10 font-palanquin text-8xl max-sm:text-[72px]
-        max-sm:leading-[82px]
-        font-bold "
-        >
-          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
-            The New Arrival
-          </span>
-          <br />
-          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
-        </h1>
-        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
-          Discover stylish Nike arrivals, quality comfort, and innovation for
-          your active life.
-        </p>
-        <Button label="Shop now" iconUrl={arrowRight} />
+        <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28'>
+          <p className='text-xl font-montserrat text-coral-red'>
+            Our Summer collections
+          </p>
 
-        <div className="flex justify-start  items-start flex-wrap w-full mt-20 gap-16">
-          {statistics.map((stat) => (
-            <div key={stat.label}>
-              <p className="text-4xl font-montserrat font-bold">{stat.value}</p>
-              <p className="leading-7 text-slate-gray font-montserrat">
-                {stat.label}
-              </p>
-            </div>
-          ))}
+          <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
+            <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>
+              The New Arrival
+            </span>
+            <br />
+            <span className='text-coral-red inline-block mt-3'>Nike</span> Shoes
+          </h1>
+          <p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm'>
+            Discover stylish Nike arrivals, quality comfort, and innovation for
+            your active life.
+          </p>
+
+          <Button label='Shop now' iconURL={arrowRight} />
+
+          <div className='flex justify-start items-start flex-wrap w-full mt-20 gap-16'>
+            {statistics.map((stat, index) => (
+              <div key={index}>
+                <p className='text-4xl font-montserrat font-bold'>{stat.value}</p>
+                <p className='leading-7 font-montserrat text-slate-gray'>
+                  {stat.label}
+                </p>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+
+        <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center'>
         <img
           src={bigShoeImage}
           alt="shoe1"
@@ -59,19 +52,25 @@ const Hero = () => {
           height={500}
           className="relative object-contain z-10"
         />
-        <div className="flex absolute sm:gap-6 gap-4 -bottom-[5%] sm:left-[10%] max-sm:px-6 transition-all">
-          {shoes.map((shoe) => (
-            <div key={shoe.id}>
-              <ShoeCard
-                imgUrl={shoe}
-                changeBigShoeImage={setBigShoeImg}
-                bigShoeImage={bigShoeImage}
-              />
-            </div>
-          ))}
+
+          <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
+            {shoes.map((shoe) => (
+              <div key={shoe.id}>
+                <ShoeCard
+                  imgUrl={shoe}
+                  changeBigShoeImage={setBigShoeImg}
+                  bigShoeImage={bigShoeImage}
+                />
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+
+
+
+
+
   );
 };
 
